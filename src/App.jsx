d8453dc3f3bd39b7d31e8ea397a45d318411c7c2
Roxi96 <roxnazapc@outlook.com>
2022-05-23 21:47:41 -0600

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <>
+     {if (edad >= 18) {
+
+     } 'eres mayor de edad' : 'no eres mayor de edad'}
+      <div>
+        <h1>Hola mundo</h1>
+         <p>soy un parrafo</p>
+      </div>
+      <div>
+        <h1>Hola mundo</h1>
+         <p>soy un parrafo</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
